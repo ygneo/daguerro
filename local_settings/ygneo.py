4a7c404b449d6992399d703af4e628285033c19d
Antonio Barcia García <antonio.barcia@gmail.com>
2012-02-26 12:14:32 +0100

    Added ygneo local settings

---
+++ local_settings/ygneo.py
@@ -0,0 +1,9 @@
+DATABASE_NAME = 'barres'
+DATABASE_USER = 'root'
+DATABASE_PASSWORD = 'root'
+
+MEDIA_ROOT = '/home/ygneo/dev/barres/media/'
+MEDIA_URL = 'http://localhost:8000/media/'
+
+TINYMCE_JS_URL = MEDIA_URL + 'daguerro/js/tiny_mce/tiny_mce.js'
+TINYMCE_JS_ROOT = MEDIA_ROOT + 'daguerro/js/tiny_mce'
