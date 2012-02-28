2590b0db7fcb9d7d51ea7ba03098dd41e721049a
Antonio Barcia García <antonio.barcia@gmail.com>
2012-02-28 23:39:52 +0100

    approach to search options layout

--- website/forms.py
+++ website/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from form_utils.forms import BetterForm
 from django.utils.translation import ugettext as _
+from photologue.models import Gallery
 
 class ShoppingCartForm(forms.Form):
     pass
@@ -13,24 +14,20 @@ class SearchOptionsForm(BetterForm):
     tags = forms.BooleanField(required=False, initial=True, label=_("Tags"),)
     caption = forms.BooleanField(required=False, initial=False, label=_("Caption"))
     location_title = forms.BooleanField(required=False, initial=False, label=_("Location"))
-    galleries = forms.IntegerField(required=False, label=_("Galleries"))
-
-    # Buscar en 
-    # [] Titulo [] Nom cient [] Familia [] Etiquetas
-    # [] Pie de foto  [] Localiz
-    # Categorias 
-    # [] 
-    #   []
+    galleries = forms.ModelMultipleChoiceField(
+        queryset=Gallery.objects.all(), 
+        widget=forms.widgets.SelectMultiple(attrs = {'size': 2}),
+        label=_('Category(s)'),
+        )
     
     class Meta:
         fields = ['title', 'alternative_title', 'family',
                   'caption', 'tags', 'location_title', 
                   'galleries']
         fieldsets = [('default-fields',
-                      {'legend': _('Search in'),
-                       'fields': ['title', 
-                                  'alternative_title',
-                                  'family', 'tags']}),
+                      { 'fields': ['title', 
+                                   'alternative_title',
+                                   'family', 'tags']}),
                      ('advanced-fields',
                       {'fields': ['caption', 
                                   'location_title']}),
