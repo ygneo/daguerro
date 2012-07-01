026be6b31faafc104a851b9a1690ee0212423648
Antonio Barcia García <antonio.barcia@gmail.com>
2012-07-02 00:41:07 +0200

    Improved add_gallery_parents_as_tags.py

--- photologue/management/commands/add_gallery_parents_as_tags.py
+++ photologue/management/commands/add_gallery_parents_as_tags.py
@@ -9,10 +9,16 @@ class Command(BaseCommand):
     requires_model_validation = True
 
     def handle(self, *args, **options):
+        count = 0 
         for photo in Photo.objects.all():
+            photo_tags_changed = False
+            count += 1
             for gallery in photo.galleries.all():
                 title = gallery.title.lower()
                 if title not in photo.tags:
+                    photo_tags_changed = True
                     photo.tags = photo.tags + " " + title
+            if photo_tags_changed:
                 photo.save()
+        print "Procesed", count, "photos."
 
