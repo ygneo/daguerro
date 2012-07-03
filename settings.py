f35ff88f84ba5a5fc6dfbd3f3ee7a8651196ba41
Antonio Barcia García <antonio.barcia@gmail.com>
2012-07-04 00:30:44 +0200

    Added robots.txt

--- settings.py
+++ settings.py
@@ -99,9 +99,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    "/var/www/barres/templates",
-    "/var/www/barres/daguerro/templates",
-    "/var/www/barres/search/templates",
+    os.path.join(os.path.dirname(__file__), 'templates')
 )
 
 
--- urls.py
+++ urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls.defaults import *
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf import settings
+from django.views.generic.simple import direct_to_template
 
 urlpatterns = patterns('',
                        (r'^daguerro/', include('daguerro.urls')),
                        (r'^tinymce/', include('tinymce.urls')),
+                       (r'^robots\.txt$', direct_to_template,
+                        {'template': 'robots.txt', 'mimetype': 'text/plain'}),
                        (r'', include('barres.website.urls')),
 )
 
