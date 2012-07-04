837f1cd8cbd02a3948ddb757111cbdf33ca368a9
Antonio Barcia García <antonio.barcia@gmail.com>
2012-07-05 00:27:23 +0200

    robots.txt and favicon.ico

--- website/urls.py
+++ website/urls.py
@@ -4,6 +4,7 @@ from django.conf.urls.defaults import patterns, include, url
 from django.conf import settings
 from django.views.generic.simple import direct_to_template
 from website.views import SearchPhotosView
+from django.views.generic.simple import redirect_to
 
 js_info_dict = {
     'packages': ('website',),
@@ -17,6 +18,7 @@ urlpatterns = urlpatterns + patterns('website.views',
                                          name='website-search-photos'),
                                      url(r'^solicitar-fotos/$', 'send_request_photos', 
                                          name='website-send-request-photos'),
+                                     (r'^favicon.ico$', redirect_to, {'url':'/static/website/img/favicon.ico'}),
                                      url(r'(?P<gallery_slugs>.+)/foto/(?P<photo_slug>.+)$', 'photo', 
                                          name='website-photo'),
                                      url(r'(?P<slugs>.+)$', 'gallery', name='website-gallery'),
