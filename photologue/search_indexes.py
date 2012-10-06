06e874f0b9c610d8cf4df22669f82dd5c626fdbf
Antonio Barcia García <antonio.barcia@gmail.com>
2012-10-06 21:37:48 +0200

    Bugfixed is_public filtering in haystack index

--- photologue/search_indexes.py
+++ photologue/search_indexes.py
@@ -4,11 +4,12 @@ from custom_fields.indexes import CustomFieldsIndex
 from haystack.query import SearchQuerySet
 
 
-class PhotoIndex(CustomFieldsIndex, indexes.Indexable):
+class PhotoIndex(CustomFieldsIndex, indexes.Indexable, indexes.RealTimeSearchIndex):
     text = indexes.CharField(document=True, model_attr='title')
     caption = indexes.CharField(model_attr='caption', null=True)
     location_title = indexes.CharField(model_attr='location_title', null=True)
     tags = indexes.CharField(null=True)
+    is_public = indexes.BooleanField(model_attr='is_public')
     galleries_ids = indexes.MultiValueField(null=True)
 
     def get_model(self):
@@ -23,6 +24,8 @@ class PhotoIndex(CustomFieldsIndex, indexes.Indexable):
     def prepare_tags(self, obj):
         return ",".join([tag for tag in obj.tags.split(" ")])
 
+    def prepare_is_public(self, obj):
+        return '' if not obj.is_public else 'True'
 
 class PhotoSearchQuerySet(SearchQuerySet, PhotoQuerySet):
     pass
