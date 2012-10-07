61e2abbf70fda87347a9c0a396ceb00b9f97a806
Antonio Barcia García <antonio.barcia@gmail.com>
2012-10-07 13:10:46 +0200

    Correct number of photos and current year in website base template

--- website/context_processors.py
+++ website/context_processors.py
@@ -1,4 +1,3 @@
-from website.urls import urlpatterns
 from photologue.models import Gallery, Photo
 
 
@@ -6,7 +5,7 @@ def header_info(request):
     extra_context = {}
     if 'daguerro' not in request.path:
         root_galleries = Gallery.objects.filter(parent=None, is_public=True)
-        photo_count = Photo.objects.public().count()
+        photo_count = Photo.objects.exclude_gallery_thumbs().public().count()
         extra_context = {'galleries': root_galleries, 'photo_count': photo_count}
     return extra_context
 
