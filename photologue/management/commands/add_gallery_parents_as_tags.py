effd6dfa0b45bf4d2e38cffbfeed904ea93524b4
Antonio Barcia García <antonio.barcia@gmail.com>
2012-10-14 19:46:50 +0200

    Deleted unneeded import

--- photologue/management/commands/add_gallery_parents_as_tags.py
+++ photologue/management/commands/add_gallery_parents_as_tags.py
@@ -1,5 +1,4 @@
-from mptt.models import MPTTModelBase
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
 from photologue.models import Photo
 
 
