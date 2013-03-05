7c953ad6242e979445bd18bb8687c0df9f9789f5
ygneo <antonio.barcia@gmail.com>
2013-03-05 23:41:38 +0100

    Improve fabfile

--- fabfile.py
+++ fabfile.py
@@ -1,7 +1,9 @@
 from fabric.api import *
 
 env.hosts = ['ygneo@barresfotonatura.com']
-env['project_path'] = "~/django_projects/barres-site/barres"
+env['project_path'] = "~/django_projects/barres-site/barres/"
+env['python_path'] = "/home/ygneo/.virtualenvs/daguerro/bin/python"
+env['pip_path'] = "/home/ygneo/.virtualenvs/daguerro/bin/pip"
 
 
 def git_status():
@@ -21,7 +23,13 @@ def reload_apache():
 
 def release():
     pushpull()
+    run('%s install -r %spip_requirements.txt' % 
+        (env['pip_path'], env['project_path']))
     with cd(env['project_path']):
-        run('./manage.py migrate') 
-        run('./manage.py collectstatic') 
+        _run_manage('migrate') 
+        _run_manage('collectstatic') 
     reload_apache()
+
+
+def _run_manage(command):
+    run("%s ./manage.py %s" % (env['python_path'], command))
