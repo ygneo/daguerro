3bb00c10015c2a7fad003997e491a56853ab4154
ygneo <antonio.barcia@gmail.com>
2017-10-17 19:24:09 +0200

    Use space as tags delimiter.

--- photologue/urls.py
+++ photologue/urls.py
@@ -32,5 +32,3 @@ urlpatterns += patterns('django.views.generic.list_detail',
     url(r'^photo/(?P<slug>[\-\d\w]+)/$', 'object_detail', {'slug_field': 'title_slug', 'queryset': Photo.objects.filter(is_public=True)}, name='pl-photo'),
     url(r'^photo/page/(?P<page>[0-9]+)/$', 'object_list', {'queryset': Photo.objects.filter(is_public=True), 'allow_empty': True, 'paginate_by': 20}, name='pl-photo-list'),
 )
-
-
