7dc56779c9ea10387e79d4e63c831504f8ec223e
ygneo <antonio.barcia@gmail.com>
2017-11-16 19:55:34 +0100

    Fix

--- website/views.py
+++ website/views.py
@@ -107,7 +107,7 @@ def _send_request_emails(subject, data, sender_email, photo_items):
                                                             django_settings.get("DAG_SALES_EMAIL")}
         _send_html_mail(EmailMultiAlternatives(django_settings.get('DAG_CONFIRMATION_MAIL_SUBJECT'),
                                                confirmation_body,
-                                               settings.EMAIL_USER,
+                                               settings.EMAIL_HOST_USER,
                                                [sender_email],)
                         )
     except BadHeaderError:
