0b8152fce02c3baae4ed1f9aeac04b46fa5cbf9a
ygneo <antonio.barcia@gmail.com>
2017-11-16 17:57:39 +0100

    Drop reading EMAIL settings from DB.
    Having STMP password in plain text in DB is baaaaad.

--- daguerro/settings.py
+++ daguerro/settings.py
@@ -1,21 +1,9 @@
 from django.conf import settings
-from django.utils.translation import ugettext as _
-from django.db.utils import DatabaseError
 import django_settings
-from django_settings.models import Setting
-from django import forms
 
 
 try:
     dag_results_per_page = django_settings.get('DAG_RESULTS_PER_PAGE', default=20)
     settings.HAYSTACK_SEARCH_RESULTS_PER_PAGE = dag_results_per_page
-except:
-    pass
-
-
-try:
-    settings.EMAIL_HOST = django_settings.get('DAG_SMTP_HOST')
-    settings.EMAIL_HOST_USER = str(django_settings.get('DAG_SMTP_HOST_USER'))
-    settings.EMAIL_HOST_PASSWORD = str(django_settings.get('DAG_SMTP_PASSWORD'))
 except:
     pass
